0582641a9e269441798ad84fd61238512ed3ce8f
otomarukanta <kanta208@gmail.com>
2022-11-27 14:48:56 +0900

    fix

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-sudo -u nginx kdir -p $MNT_DIR
+mkdir -p $MNT_DIR
+chown nginx:nginx $MNT_DIR
 
 echo "Mounting GCS Fuse."
 sudo -u nginx /root/go/bin/gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
@@ -9,7 +10,4 @@ echo "Mounting completed."
 
 ls -la $MNT_DIR
 
-# exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app &
-# 
-# wait -n
 nginx -g "daemon off;"
