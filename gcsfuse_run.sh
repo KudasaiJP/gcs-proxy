0731f69bac8c75c73903ad5824a25a5aaab2c8d3
otomarukanta <kanta208@gmail.com>
2022-11-28 01:26:50 +0900

    fix

--- gcsfuse_run.sh
+++ gcsfuse_run.sh
@@ -2,15 +2,18 @@
 set -eo pipefail
 
 mkdir -p $MNT_DIR
-chown nginx:nginx $MNT_DIR
 
 ls -la $MNT_DIR
 
 echo $GOOGLE_APPLICATION_CREDENTIALS
 
 echo "Mounting GCS Fuse."
-/bin/gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
+/bin/gcsfuse -o allow_other --debug_gcs --debug_fuse $BUCKET $MNT_DIR 
 echo "Mounting completed."
+
+mkdir -p $MNT_DIR/about
+mkdir -p $MNT_DIR/assets
+mkdir -p $MNT_DIR/stickers
 
 ls -la $MNT_DIR
 
